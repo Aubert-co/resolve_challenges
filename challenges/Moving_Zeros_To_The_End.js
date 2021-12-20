92843ed8f6499c43e033ebaf22146e969bfb5e99
Black <53127242+Aubert-co@users.noreply.github.com>
2021-12-19 23:23:34 -0300

    Create Moving_Zeros_To_The_End.js

---
+++ challenges/Moving_Zeros_To_The_End.js
@@ -0,0 +1,17 @@
+var moveZeros = function (a) {
+    return  a.sort((v,b)=>{
+      if(v===0){
+          return  v =1
+       }
+       if(b>0){
+           return !b
+       }
+       if(typeof v === 'string' ){
+         
+           return v= a.sort()
+       }
+       return 
+       })  
+  
+      
+  }
