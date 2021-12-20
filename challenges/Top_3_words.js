185aafcb07319b8a4d0fad2db6e1a885b81ba7a7
Black <53127242+Aubert-co@users.noreply.github.com>
2021-12-19 23:57:11 -0300

    Update Top_3_words.js

--- challenges/Top_3_words.js
+++ challenges/Top_3_words.js
@@ -40,12 +40,7 @@ function topThreeWords(text) {
    }).filter((val)=>val!=='')
    
 }
-console.log(topThreeWords(`In a village of La Mancha, the name of which I have no desire to call to
-mind, there lived not long since one of those gentlemen that keep a lance
-in the lance-rack, an old buckler, a lean hack, and a greyhound for
-coursing. An olla of rather more beef than mutton, a salad on most
-nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
-on Sundays, made away with three-quarters of his income.`))
+console.log(topThreeWords('\ e e E E a a A A AA '))
 //const ss =  ", e m & % $ . \ A won't"
 
 //const a = ss.replace(/[$%¨&*()/!"`{}?<>., ]/gi," ")
