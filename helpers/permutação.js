e50b450e54c6287b95bb79a89afa61d7ea7a0cc2
Black <53127242+Aubert-co@users.noreply.github.com>
2022-09-01 23:22:06 -0300

    Create permutação.js

---
+++ helpers/permutação.js
@@ -0,0 +1,25 @@
+/*var fatorial = [1,2,3,4].length
+var resultado = fatorial;
+for (var i = 1; i < fatorial; i++) {
+    resultado *= i;
+}
+console.log(resultado)
+
+ * 2 [1,2]
+ * [1,1]
+ * [2]
+ * 
+ * 3
+ * [1,2,3]
+ * [1,1,1]
+ * [1,2]
+ * [3]
+ */
+const ar = [1,2]
+
+const vs = (n)=>{
+    for(let i ;i<n;i++){
+        
+    }
+
+}
