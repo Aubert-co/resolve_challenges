2d4d5e1722d1dc142a56eb035a681ff3ee81faf4
Black <53127242+Aubert-co@users.noreply.github.com>
2022-09-03 23:39:34 -0300

    Create separeArray.js

---
+++ helpers/separeArray.js
@@ -0,0 +1,14 @@
+const array = [1,2,3,4]
+const counts =(ar,n)=>{
+const b = []
+let count = 0
+for(let i=0;i<ar.length*2;i++,count++){
+
+    
+    if(count >= n)count=0
+    
+}
+return b
+}
+
+console.log(counts(array,2))
