b35eadd19fcb9b3e8c9102c9600730ad8b30166c
Black <53127242+Aubert-co@users.noreply.github.com>
2022-09-04 23:56:19 -0300

    Update permutation.js

--- helpers/permutation.js
+++ helpers/permutation.js
@@ -1,28 +1,26 @@
-const ff = (ar,len)=>{
-    const ae=[] , av =[]
-    ar.map((val,ind)=>{
-    
-    })
-}
+const v = []
 const func = (arr,k)=>{
     var len = arr.length
     
     if(k===len){
-        console.log(arr)
-        return
+       v.push(arr)
+   
+       return
     }
     for(let i=k;i<len;i++){
         arr = TrocaCaractere(arr,k,i)
         func(arr,k+1)
+      
     }
-
+    return v
 }
 function TrocaCaractere(str, p1, p2) //ela recebe três parâmetros
 { //str: o vetor de dados; p1: indice do dado1; p2: indice do dado2
     var tmp = str[p1];
     str[p1] = str[p2];
     str[p2] = tmp;
+
     return str;
 }
-func([1,2],0)
+func([1,2,3,4],0)
 
