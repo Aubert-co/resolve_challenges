4c25a933a562c728e67a93cbb7c09ad31c346375
Black <53127242+Aubert-co@users.noreply.github.com>
2022-09-22 22:22:49 -0300

    Update moving0.js

--- codewars/NV5/moving0.js
+++ codewars/NV5/moving0.js
@@ -1,17 +1,32 @@
-const arr = [0,0,'a',0,1,2,3,4,5,'7',7]
+const arr = ['a',1,2,3,'0',4,5,'7',7]
 
-const mov = (a)=>{
-    const copy = [...a]
+const moveZeros = (a)=>{
     var count  = 0
-    const zeros = arr.map((val)=>{
-        if(val === 0){
-            count++
-            return ''
-        }
-        return val
-    }).filter((val)=>val!=='')
-    zeros.fill(0,zeros.length+1,count)
-    console.log(zeros)
+    const zeros = a.filter((val)=>{
+        if(val === 0)count++
+        return val!==0
+    })
+    const asv = new Array(count).fill(0)
+    zeros.push(...asv)
+    return zeros
 }
+console.log(moveZeros(arr))
+/**
+ * var moveZeros = function (a) {
+  return  a.sort((v,b)=>{
+    if(v===0){
+        return  v =1
+     }
+     if(b>0){
+         return !b
+     }
+     if(typeof v === 'string' ){
+       
+         return v= a.sort()
+     }
+     return 
+     })  
 
-mov(arr)
+    
+}
+ */
