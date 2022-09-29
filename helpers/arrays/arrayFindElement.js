cea7f2c94880e667ec2ecb6afd6bbc3da5bb730d
Black <53127242+Aubert-co@users.noreply.github.com>
2022-09-28 23:37:07 -0300

    Create arrayFindElement.js

---
+++ helpers/arrays/arrayFindElement.js
@@ -0,0 +1,13 @@
+const arry = [1,2,3,4,5,1,2,3,1]
+const func = (arr,find)=>{
+    const positions = []
+ 
+    if(positions.length === 0)return -1
+    return positions
+}
+
+
+const v = Array.prototype = ()=>{
+    return 'h1'
+}
+arry.v()
