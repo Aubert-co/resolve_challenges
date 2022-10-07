7996a77b78a4cc74aa682fc8cccb9a44daddb48c
Black <53127242+Aubert-co@users.noreply.github.com>
2022-10-06 23:05:11 -0300

    Create diagonal.js

---
+++ hackerank/MEDIUM/diagonal.js
@@ -0,0 +1,23 @@
+/**
+ * precio salvar um valor em uma variavel
+ */
+const arr = 
+[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+const vv = (val)=>{
+
+}
+const  f = (a)=>{
+    for(let i =0;i<a.length;i++){
+        const map = a[i].map((val)=>{
+            console.log(val)
+        })
+    
+    
+    }
+}
+
+f(arr)
