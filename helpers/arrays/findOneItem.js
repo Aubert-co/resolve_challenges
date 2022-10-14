e39010049655c9160eedea15cb898f06786beba6
Black <53127242+Aubert-co@users.noreply.github.com>
2022-10-13 22:29:09 -0300

    Update findOneItem.js

--- helpers/arrays/findOneItem.js
+++ helpers/arrays/findOneItem.js
@@ -1,10 +1,29 @@
-const vls = [1,1,2,4,4,4,5,5,5,3]
-
-const b = []
+/*const vls = [ [ 5,  3, 9, 4, 9,  3, 1 ],
+[ 1, 2, 2, 4, 4, 1 ],
+[ 1, 2, 3 ] ]
+const v = 1
+v.flat()
+*/
+/*const b = []
 const a = vls.map((vals)=>{
     const ss =vls.filter((val)=>val===vals)
     if(ss.length >1)return
     b.push(vals)
 })
-
-console.log(b)
+console.log(b.length)
+const max = b.reduce(function(a, b) {
+    return Math.max(a, b);
+  }, -Infinity);
+  console.log(max)*/
+  const vls = [1,2,2,3,4,5,56,3]
+ 
+const findRepeatValues = (array)=>{
+  const b = []
+  const a = vls.map((vals)=>{
+      const ss =vls.filter((val)=>val===vals)
+      if(ss.length >1)b.push(vals)
+      
+})
+return b
+}
+console.log(findRepeatValues(vls))
