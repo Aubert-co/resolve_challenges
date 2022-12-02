74f36448f4ad4b46726381d46e31e52561b188a2
Black <53127242+Aubert-co@users.noreply.github.com>
2022-12-01 23:26:30 -0300

    Create sumNum.js

---
+++ codewars/NV4/sumNum.js
@@ -0,0 +1,7 @@
+
+const sumStrings = (a,b)=>(Number(a)+Number(b)).toString()
+
+const value = sumStrings('712569312664357328695151392','8100824045303269669937')
+const num = 12345678901234564
+
+console.log(value)
