e8d20d87d7ea2ddb5d7e4eeb6c588f5093c19d9e
Black <53127242+Aubert-co@users.noreply.github.com>
2022-12-19 22:41:12 -0300

    Update hunt.js

--- games/hunt.js
+++ games/hunt.js
@@ -11,24 +11,24 @@ const datas = [
     {day:'11/12',sparks:0,gasto:5525.00,ganho:(3*230)+6020,baus:27},
     {day:'12/12',baus:(44-27)+(4+7),ganho:(11460-6480)+(460+460+1880),sparks:1,gasto:5645},
     {day:'13/12',baus:20,sparks:2,ganho:4880,gasto:3000},
-    {day:'14/12',baus:51-20,ganho:13035-4880,gasto:6210,sparks:1}
+    {day:'14/12',baus:51-20,ganho:13035-4880,gasto:6210,sparks:1},
+    {day:'15/12',baus:37,ganho:10410,sparks:2,gasto:6830},
+    {day:'16/12',baus:35,sparks:1,gasto:6030,ganho:10980},
+    {day:'17/12',baus:41,ganho:13600,gasto:7145,sparks:2},
+    {day:'18/12',baus:25,ganho:19075,gasto:6025,sparks:1},
+    {day:'19/12',baus:44,ganho:16895,gasto:7165,sparks:2}
 ]
-
 /**
- * after
- * Tier: queens,  TotalChests: 27,  TotalUPX: 6480.00,  Spark: 0.02 (~$12.00),  SparkChests: 1,  Profit: 2665.00,  TotalFees: 3815.00,  SparkPerTreasureRatio: 0.04,  SendsPerTreasureRatio: 2.00,  SendsUsed: 54,  AvgSendFee: 35.46
- */
-/**before
- * Tier: queens,  TotalChests: 44,  TotalUPX: 11460.00,  Spark: 0.02 (~$12.00),  SparkChests: 1,  Profit: 7645.00,  TotalFees: 3815.00,  SparkPerTreasureRatio: 0.02,  SendsPerTreasureRatio: 1.23,  SendsUsed: 54,  AvgSendFee: 35.46
+ * Tier: queens,  TotalChests: 46,  TotalUPX: 16895.00,  Spark: 0.04 (~$24.00),  SparkChests: 2,  Profit: 9730.00,  TotalFees: 7165.00,  SparkPerTreasureRatio: 0.04,  SendsPerTreasureRatio: 2.37,  SendsUsed: 109,  AvgSendFee: 33.90
  */
 const investimentos = [
-8000,6510,11000,11000,7864,6814,10800,7200,7200,6613,8500,10500
+8000,6510,11000,11000,7864,6814,10800,7200,7200,6613,8500,10500,7000,7000,47000,40000
 ]
 
 function Values (){
 
-const daylyValue = datas.map(( {ganho,baus,sparks,gasto} )=>{
-return {profit:(ganho-gasto),baus:baus+sparks}
+const daylyValue = datas.map(( {ganho,baus,sparks,gasto,day} )=>{
+return {day,profit:(ganho-gasto),baus:baus+sparks,sparks:(sparks/100)*2}
 })
 const spendValue = investimentos.reduce((vl,tr)=>vl+tr)
 const generalValue = datas.reduce((vl,tr)=>{
@@ -48,10 +48,14 @@ const median = (days)=>{
     const em_days_sparks = `em ${days} dias vc tera um lucro de ${sparks*days}`
     return {day_median:profit,dayprofy:em_days_baus,em_days_sparks,sparks_median:sparks}
 }
+const lastDays= (day)=>{
+    const split = datas.splice(datas.length-1,day)
+    return split.reduce((vl,tr)=>vl.ganho+tr.ganho)
+}
 return {
-    daylyValue,generalValue,
+    daylyValue,generalValue,lastDays,
     fund:generalValue.profit-spendValue,median
 }
 }
-const {daylyValue,generalValue,fund,median} = Values()
-console.log(generalValue)
+const {daylyValue,generalValue,fund,median,lastDays} = Values()
+console.log(daylyValue)
