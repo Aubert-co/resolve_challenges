c509d2fe998c2068b6ac5e41795003d5bd185df3
Black <53127242+Aubert-co@users.noreply.github.com>
2022-12-25 23:38:10 -0300

    Update coins.js

--- helpers/numbers/coins.js
+++ helpers/numbers/coins.js
@@ -8,19 +8,31 @@ const f = (money,coins)=>{
 
     for(let i =0;i<coins.length;i++){
         money = moneys
+        var rest = 0
         while(money > 0){
+            rest+=1
+        
             const valueToFind=   money- coins[i]
             
-            const m = coins.filter((val)=>{
+           /* const m = coins.filter((val)=>{
                 if(valueToFind===val && val !== coins[i])return val
             })
             
             if(m.length >0 )a.push([coins[i],m])
-
-            money-=i[coins]
+*/          
+            const findValues = coins.filter((val)=>{
+                const f = coins.filter((s)=>s===rest)
+                if((rest + val) === moneys && f.length >0)a.push([val,rest])
+            })
+          
+            money-=coins[i]
+         
         }
-        coins[i] = ''
+            coins[i] = ''
+          
+        money = moneys
     }
     console.log(a)
+  
 }
-f(10,[2,5,3].sort())
+f(4,[2,1].sort())
