9d8f8bc8e36d522621085ec2887380af66f46467
Black <53127242+Aubert-co@users.noreply.github.com>
2023-01-08 23:41:53 -0300

    Update hunt.js

--- games/hunt.js
+++ games/hunt.js
@@ -9,16 +9,17 @@ function Values (){
     const daylyValue = datas.map(( {ganho,baus,sparks,gasto,day} )=>{
     return {day,profit:(ganho-gasto),baus:baus+sparks,sparks:(sparks/100)*2}
     })
+
     const spendValue = investimentos.reduce((vl,tr)=>vl+tr)
     const generalValue = datas.reduce((vl,tr)=>{
-    return {
-        ganho:vl.ganho+tr.ganho,
-        baus:vl.baus+tr.baus,
-        gasto:vl.gasto+tr.gasto,
-        profit:(vl.ganho+tr.ganho)-(vl.gasto+tr.gasto),
-        sparks:vl.sparks+(tr.sparks*2)/100,
-        totalDays:datas.length
-    }
+        return {
+            ganho:vl.ganho+tr.ganho,
+            baus:vl.baus+tr.baus,
+            gasto:vl.gasto+tr.gasto,
+            profit:(vl.ganho+tr.ganho)-(vl.gasto+tr.gasto),
+            sparks:vl.sparks+(tr.sparks*2)/100,
+            totalDays:datas.length
+        }
     })
     const median = (days)=>{
         const profit = generalValue.profit/datas.length
@@ -55,4 +56,4 @@ function Values (){
 const {daylyValue,generalValue,fund,median,lastDays} = Values()
 
 //const sparks= datasProfit.filter(({profit})=>profit>5000)
-console.log(median(30))
+console.log(daylyValue)
