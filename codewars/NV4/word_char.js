e31f5815ff5bad94815de28ab64425b61b2bec4e
Black <53127242+Aubert-co@users.noreply.github.com>
2023-01-12 23:24:14 -0300

    Update word_char.js

--- codewars/NV4/word_char.js
+++ codewars/NV4/word_char.js
@@ -1,15 +1,64 @@
-const s = "You can try, but you'll never catch me.\nBazinga!"
+/*class DocumentParser {
+    constructor() {
+      this.wordCount = 0;
+      this.charCount = 0;
+      this.lineCount = 0;
+    }
+    parse(text) {
+     
+      let lines = text.split('\n');
+  
+      // Atualizar o contador de linhas
+      this.lineCount = lines.length;
+  
+      // Percorrer cada linha do texto
+      for (let line of lines) {
+        // Remover espaços em branco no início e no final da linha
+        line = line.trim();
+  
+        // Atualizar o contador de caracteres
+        this.charCount += line.length;
+  
+        // Quebrar a linha em palavras usando o método split
+        let words = line.split(' ');
+  
+        // Atualizar o contador de palavras
+        this.wordCount += words.length;
+      }
+    }
+  }
+*/
+  function DocumentParser(reader)
+{
+  this.reader = reader;
+  this.reset();
+  this.wordCount = 0;
+  this.charCount = 0;
+  this.lineCount = 0;
 
-const rep = s.replace('\n','*')
+  this.parser()
+}
+DocumentParser.prototype.parser = function(){
 
-const datas = s.split('').filter((val)=>val === '\n')
+let ln = this.reader.split('\n')
+this.lineCount = ln.length
 
-//console.log(datas.length)
-//console.log(rep.split('*').join('').split(' '))
+for(let lines of ln){
+    lines = lines.trim()
+    this.charCount += lines.length
+    let w = lines.split(' ')
 
-const v =s.replace(/[.*+?^${}()|[\]\\!,\n]/g,' ')
-var count = 0
-v.split(' ').map((val)=>{
-    count+=val.length
-})
-console.log(v.split(' ').filter((val)=>val!=='').length)
+    this.wordCount += w.length
+}
+}
+DocumentParser.prototype.reset = function()
+{
+  this.wordCount = 0;
+  this.charCount = 0;
+  this.lineCount = 0;
+};
+
+const parser =  new DocumentParser("Hello World")
+parser.parser("Hello World")
+
+console.log(parser.charCount)
