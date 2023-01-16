9a412b07a901eede03c11f88385c0a019c6cfc9a
Black <53127242+Aubert-co@users.noreply.github.com>
2023-01-15 23:16:10 -0300

    Update hunt.js

--- games/hunt.js
+++ games/hunt.js
@@ -56,4 +56,4 @@ function Values (){
 const {daylyValue,generalValue,fund,median,lastDays} = Values()
 
 //const sparks= datasProfit.filter(({profit})=>profit>5000)
-console.log(fund)
+console.log(lastDays(15))
