c1fcc5cfa175d16bed44bef9ed8ce81b3f9d33f2
Black <53127242+Aubert-co@users.noreply.github.com>
2023-01-17 23:44:16 -0300

    Update reverse.js

--- codewars/NV4/reverse.js
+++ codewars/NV4/reverse.js
@@ -1,33 +1,39 @@
-const string = 'a-c*=b'
+const value = 'v-a=e-p*m&'
 
 
 
-const f = (v)=>{
-    const b = []
-    v = v.reverse()
-   for(let i =0;i<v.length;i++){
-        if(v[i].match(/[a-zA-Z]/i)){
-            b.push(v[i])
-           
-        }else{
-            b.push('')
-        }
-   }
-    const s = v.toString().replace(/[a-zA-Z]/gi,' ')
+const c = (s)=>{
+  const lett =  s.split('')
+  const ss = lett.toString().replace(/[a-zA-Z]/gi,' ')
     
-    const fv = s.split(',').filter((val)=>val!==' ')
-   
-   const bs = b.map((val)=>{
-    if(val === ''){
-           console.log(fv)
-        val =fv[0]
-        fv.shift()
-     
+    const fv = ss.split(',').filter((val)=>val!==' ')
+    const rev = value.match(/[a-zA-Z]/gi).reverse()
+    const obj = {}
+  
+    for(let i =0;i<s.length;i++){
+        if(!s[i].match(/[a-zA-Z]/i)){
+            obj[i] = s[i]
+        }
     }
-    return val
-   })
-   console.log(bs)
-   
-   
+    const ar = new Array(rev.length+Object.keys(obj).length)
+    let vi = 0
+    for(let i =0 ;i<ar.length;i++ ,vi++){
+        
+        if(obj[i]){
+        
+            ar[i] = obj[i]
+           
+            if(rev[vi])ar[i+1] = rev[vi]
+            i+=1
+        }
+            if(rev[vi]){
+                ar[i] = rev[vi]
+            }
+        
+  
+    }
+    const compare = ar.join('')
+
+    console.log(rev.join(''))
 }
-f(string.split(''))
+console.log(c(value))
