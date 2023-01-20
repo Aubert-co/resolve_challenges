a29962a2caabaf6e6d2701503afc1c821f959645
Black <53127242+Aubert-co@users.noreply.github.com>
2023-01-19 23:45:13 -0300

    Update datas.js

--- games/datas.js
+++ games/datas.js
@@ -46,5 +46,10 @@ module.exports   = [
     {day:'15/1/23',baus:38,sparks:1,ganho:12680,gasto:6565},
     {day:'16/1/23',baus:39,sparks:5,ganho:10025,gasto:6945},
     {day:'17/1/23',baus:36,sparks:0,ganho:11260,gasto:6685},
-    {day:'18/1/23',baus:38,ganho:13500,sparks:2,gasto:6530}
+    {day:'18/1/23',baus:38,ganho:13500,sparks:2,gasto:6530},
+    {day:'19/1/23',baus:29,ganho:12590,gasto:5740,sparks:2}
 ]
+
+/***
+ * Tier: queens,  TotalChests: 29,  TotalUPX: 12590.00,  Spark: 0.04 (~$24.00),  SparkChests: 2,  Profit: 6850.00,  TotalFees: 5740.00,  SparkPerTreasureRatio: 0.07,  SendsPerTreasureRatio: 2.86,  SendsUsed: 83,  AvgSendFee: 34.94
+ */
