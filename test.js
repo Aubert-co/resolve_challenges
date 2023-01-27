8c0611df150100d730f50ea0ca1d39bdc199a598
Black <53127242+Aubert-co@users.noreply.github.com>
2023-01-26 23:37:00 -0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,406 @@
+const v = [
+    {
+        "user": {
+            "eos_id": "bciibobe2mhm",
+            "username": "sparrow1984",
+            "image": "https://static.upland.me/avatars/exclusive/2022 Poland Mug.svg",
+            "color": "#10C126",
+            "is_in_jail": false
+        },
+        "treasure": {
+            "color": "#5F65FE",
+            "type": "box",
+            "is_riot_mode": false,
+            "reward": 0.03,
+            "reward_type": "spark",
+            "locked_at": "2023-01-26T20:35:59.135Z",
+            "spawn_at": "2023-01-26T20:35:27.959Z",
+            "full_address": "1351 145TH PL, Queens, NY",
+            "image": "grey_spark_chest"
+        }
+    },
+    {
+        "user": {
+            "eos_id": "kvagfa3tgj2g",
+            "username": "leozinf1el",
+            "image": "https://static.upland.me/avatars/exclusive/FIFA_Fireball.svg",
+            "color": "#FAFF00",
+            "is_in_jail": false
+        },
+        "treasure": {
+            "color": "#76379D",
+            "type": "pinata",
+            "is_riot_mode": true,
+            "reward": 1247,
+            "reward_type": "upx",
+            "locked_at": "2023-01-26T20:25:43.185Z",
+            "spawn_at": "2023-01-26T20:24:26.655Z",
+            "full_address": "12605 CRONSTON AVE, Queens, NY",
+            "image": "pineapple"
+        }
+    },
+    {
+        "user": {
+            "eos_id": "vmyqwrjnumzx",
+            "username": "gobie88",
+            "image": "https://static.upland.me/avatars/pro/Red+Truck.svg",
+            "color": "#30EFFF",
+            "is_in_jail": false
+        },
+        "treasure": {
+            "color": "#76379D",
+            "type": "pinata",
+            "is_riot_mode": true,
+            "reward": 843,
+            "reward_type": "upx",
+            "locked_at": "2023-01-26T20:13:26.639Z",
+            "spawn_at": "2023-01-26T20:11:34.618Z",
+            "full_address": "11809 201ST PL, Queens, NY",
+            "image": "star"
+        }
+    },
+    {
+        "user": {
+            "eos_id": "so2srevihebl",
+            "username": "rockdrigo",
+            "image": "https://static.upland.me/avatars/exclusive/adult_midnight_vampire.svg",
+            "color": "#B86EFF",
+            "is_in_jail": false
+        },
+        "treasure": {
+            "color": "#5F65FE",
+            "type": "box",
+            "is_riot_mode": false,
+            "reward": 0.03,
+            "reward_type": "spark",
+            "locked_at": "2023-01-26T20:08:27.947Z",
+            "spawn_at": "2023-01-26T20:07:57.546Z",
+            "full_address": "2943 166TH ST, Queens, NY",
+            "image": "grey_spark_chest"
+        }
+    },
+    {
+        "user": {
+            "eos_id": "zwbxuaewbphd",
+            "username": "babyfin",
+            "image": "https://static.upland.me/avatars/exclusive/babyfin.svg",
+            "color": "#30EFFF",
+            "is_in_jail": false
+        },
+        "treasure": {
+            "color": "#FFA800",
+            "type": "box",
+            "is_riot_mode": false,
+            "reward": 0.05,
+            "reward_type": "spark",
+            "locked_at": "2023-01-26T20:07:24.147Z",
+            "spawn_at": "2023-01-26T20:06:03.484Z",
+            "full_address": "13827 226TH ST, Queens, NY",
+            "image": "spark_exclusive_chest"
+        }
+    },
+    {
+        "user": {
+            "eos_id": "lce5k2jgyqye",
+            "username": "jeffrey00",
+            "image": "https://static.upland.me/avatars/visitors/_Squared+Tree+1.svg",
+            "color": "#30C5EB",
+            "is_in_jail": false
+        },
+        "treasure": {
+            "color": "#76379D",
+            "type": "pinata",
+            "is_riot_mode": true,
+            "reward": 732,
+            "reward_type": "upx",
+            "locked_at": "2023-01-26T19:52:34.604Z",
+            "spawn_at": "2023-01-26T19:51:23.608Z",
+            "full_address": "4215 160TH ST, Queens, NY",
+            "image": "star"
+        }
+    },
+    {
+        "user": {
+            "eos_id": "uuonxbvsvdu5",
+            "username": "cryptovenom",
+            "image": "https://static.upland.me/avatars/exclusive/cryptovenom.svg",
+            "color": "#00838C",
+            "is_in_jail": false
+        },
+        "treasure": {
+            "color": "#5F65FE",
+            "type": "box",
+            "is_riot_mode": false,
+            "reward": 0.04,
+            "reward_type": "spark",
+            "locked_at": "2023-01-26T19:39:57.530Z",
+            "spawn_at": "2023-01-26T19:39:14.621Z",
+            "full_address": "12617 148TH ST, Queens, NY",
+            "image": "blue_spark_chest"
+        }
+    },
+    {
+        "user": {
+            "eos_id": "jdgnso1vdoyt",
+            "username": "brizio13",
+            "image": "https://static.upland.me/avatars/exclusive/WC_trophy.svg",
+            "color": "#FAFF00",
+            "is_in_jail": false
+        },
+        "treasure": {
+            "color": "#76379D",
+            "type": "box",
+            "is_riot_mode": true,
+            "reward": 720,
+            "reward_type": "upx",
+            "locked_at": "2023-01-26T19:25:23.580Z",
+            "spawn_at": "2023-01-26T19:24:45.426Z",
+            "full_address": "4409 54TH DR, Queens, NY",
+            "image": "basic"
+        }
+    },
+    {
+        "user": {
+            "eos_id": "hf2mesq45rle",
+            "username": "dawgfanusa",
+            "image": "https://static.upland.me/avatars/exclusive/stakor_terraformer_head.svg",
+            "color": "#FAFF00",
+            "is_in_jail": false
+        },
+        "treasure": {
+            "color": "#5F65FE",
+            "type": "box",
+            "is_riot_mode": false,
+            "reward": 0.06,
+            "reward_type": "spark",
+            "locked_at": "2023-01-26T19:22:14.600Z",
+            "spawn_at": "2023-01-26T19:21:33.504Z",
+            "full_address": "10402 227TH ST, Queens, NY",
+            "image": "metal_spark_chest"
+        }
+    },
+    {
+        "user": {
+            "eos_id": "htjdw3syckid",
+            "username": "grud09",
+            "image": "https://static.upland.me/avatars/exclusive/grud09.svg",
+            "color": "#30C5EB",
+            "is_in_jail": false
+        },
+        "treasure": {
+            "color": "#76379D",
+            "type": "box",
+            "is_riot_mode": true,
+            "reward": 1200,
+            "reward_type": "upx",
+            "locked_at": "2023-01-26T19:05:45.410Z",
+            "spawn_at": "2023-01-26T19:04:27.558Z",
+            "full_address": "24455 88TH DR, Queens, NY",
+            "image": "basic"
+        }
+    },
+    {
+        "user": {
+            "eos_id": "gpnor5l1hinz",
+            "username": "ownhouse",
+            "image": "https://static.upland.me/avatars/exclusive/queens.svg",
+            "color": "#10C126",
+            "is_in_jail": false
+        },
+        "treasure": {
+            "color": "#5F65FE",
+            "type": "box",
+            "is_riot_mode": false,
+            "reward": 0.03,
+            "reward_type": "spark",
+            "locked_at": "2023-01-26T19:05:33.486Z",
+            "spawn_at": "2023-01-26T19:05:13.795Z",
+            "full_address": "10422 113TH ST, Queens, NY",
+            "image": "grey_spark_chest"
+        }
+    },
+    {
+        "user": {
+            "eos_id": "hbfgcy4vmtyd",
+            "username": "wattalife",
+            "image": "https://static.upland.me/avatars/exclusive/cool_gnome.svg",
+            "color": "#FFA34D",
+            "is_in_jail": false
+        },
+        "treasure": {
+            "color": "#5F65FE",
+            "type": "box",
+            "is_riot_mode": false,
+            "reward": 0.04,
+            "reward_type": "spark",
+            "locked_at": "2023-01-26T18:46:13.776Z",
+            "spawn_at": "2023-01-26T18:45:45.377Z",
+            "full_address": "7109 72ND PL, Queens, NY",
+            "image": "blue_spark_chest"
+        }
+    },
+    {
+        "user": {
+            "eos_id": "mea123zbhdfr",
+            "username": "miguelcc",
+            "image": "https://static.upland.me/avatars/pro/Flowers.svg",
+            "color": "#FAFF00",
+            "is_in_jail": false
+        },
+        "treasure": {
+            "color": "#76379D",
+            "type": "pinata",
+            "is_riot_mode": false,
+            "reward": 409,
+            "reward_type": "upx",
+            "locked_at": "2023-01-26T18:41:27.539Z",
+            "spawn_at": "2023-01-26T18:40:29.628Z",
+            "full_address": "3727 74TH ST, Queens, NY",
+            "image": "star"
+        }
+    },
+    {
+        "user": {
+            "eos_id": "doz4lc4dkc4q",
+            "username": "danyp2000",
+            "image": "https://static.upland.me/avatars/exclusive/WC_trophy.svg",
+            "color": "#00465F",
+            "is_in_jail": false
+        },
+        "treasure": {
+            "color": "#5F65FE",
+            "type": "box",
+            "is_riot_mode": false,
+            "reward": 0.04,
+            "reward_type": "spark",
+            "locked_at": "2023-01-26T18:28:45.354Z",
+            "spawn_at": "2023-01-26T18:28:14.899Z",
+            "full_address": "16335 86TH ST, Queens, NY",
+            "image": "blue_spark_chest"
+        }
+    },
+    {
+        "user": {
+            "eos_id": "utlcb52wcjnf",
+            "username": "0rg3n27",
+            "image": "https://static.upland.me/avatars/exclusive/cool_gnome.svg",
+            "color": "#FAFF00",
+            "is_in_jail": false
+        },
+        "treasure": {
+            "color": "#76379D",
+            "type": "box",
+            "is_riot_mode": false,
+            "reward": 360,
+            "reward_type": "upx",
+            "locked_at": "2023-01-26T18:27:29.609Z",
+            "spawn_at": "2023-01-26T18:26:45.693Z",
+            "full_address": "6107 56TH RD, Queens, NY",
+            "image": "basic"
+        }
+    },
+    {
+        "user": {
+            "eos_id": "dnh1hwm2gdsl",
+            "username": "snailnft",
+            "image": "https://static.upland.me/avatars/uplanders/_Turtle.svg",
+            "color": "#10C126",
+            "is_in_jail": false
+        },
+        "treasure": {
+            "color": "#FFA800",
+            "type": "box",
+            "is_riot_mode": false,
+            "reward": 0.05,
+            "reward_type": "spark",
+            "locked_at": "2023-01-26T18:20:03.455Z",
+            "spawn_at": "2023-01-26T18:19:31.970Z",
+            "full_address": "15555 116TH DR, Queens, NY",
+            "image": "spark_exclusive_chest"
+        }
+    },
+    {
+        "user": {
+            "eos_id": "odacrspatglp",
+            "username": "manumioto21",
+            "image": "https://static.upland.me/avatars/exclusive/accnorman2.svg",
+            "color": "#00838C",
+            "is_in_jail": false
+        },
+        "treasure": {
+            "color": "#5F65FE",
+            "type": "box",
+            "is_riot_mode": false,
+            "reward": 0.03,
+            "reward_type": "spark",
+            "locked_at": "2023-01-26T17:58:14.877Z",
+            "spawn_at": "2023-01-26T17:57:34.708Z",
+            "full_address": "3305 UTOPIA PKWY, Queens, NY",
+            "image": "grey_spark_chest"
+        }
+    },
+    {
+        "user": {
+            "eos_id": "v3xgsc1mynpj",
+            "username": "simobernago",
+            "image": "https://static.upland.me/avatars/exclusive/simobernago.svg",
+            "color": "#89B208",
+            "is_in_jail": false
+        },
+        "treasure": {
+            "color": "#76379D",
+            "type": "box",
+            "is_riot_mode": false,
+            "reward": 360,
+            "reward_type": "upx",
+            "locked_at": "2023-01-26T17:56:45.649Z",
+            "spawn_at": "2023-01-26T17:54:28.961Z",
+            "full_address": "15818 32ND AVE, Queens, NY",
+            "image": "basic"
+        }
+    },
+    {
+        "user": {
+            "eos_id": "vgl1reqawrpl",
+            "username": "operaghost12",
+            "image": "https://static.upland.me/avatars/exclusive/operaghost12.svg",
+            "color": "#FAFF00",
+            "is_in_jail": false
+        },
+        "treasure": {
+            "color": "#76379D",
+            "type": "pinata",
+            "is_riot_mode": false,
+            "reward": 558,
+            "reward_type": "upx",
+            "locked_at": "2023-01-26T17:37:28.919Z",
+            "spawn_at": "2023-01-26T17:36:29.673Z",
+            "full_address": "18410 89TH AVE, Queens, NY",
+            "image": "star"
+        }
+    },
+    {
+        "user": {
+            "eos_id": "joeais2rnthw",
+            "username": "doxts",
+            "image": "https://static.upland.me/avatars/exclusive/Mascot.svg",
+            "color": "#D80707",
+            "is_in_jail": false
+        },
+        "treasure": {
+            "color": "#5F65FE",
+            "type": "box",
+            "is_riot_mode": false,
+            "reward": 0.04,
+            "reward_type": "spark",
+            "locked_at": "2023-01-26T17:27:34.688Z",
+            "spawn_at": "2023-01-26T17:27:05.276Z",
+            "full_address": "21303 41ST AVE, Queens, NY",
+            "image": "blue_spark_chest"
+        }
+    }
+]
+const ad = v.map((val)=>val.treasure.full_address)
+
+console.log(ad)
+const values = [68,97,145,201,166,226,160,148,54,227,88,113,72,74,86,56,116,32,89,41]
